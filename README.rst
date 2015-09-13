b42509651728cbd92f19f0fedc100b1c318ccb47
Marco Bernasocchi <marco@bernawebdesign.ch>
2015-09-13 15:00:34 +0200

    added docker tag

--- README.rst
+++ README.rst
@@ -6,7 +6,7 @@ Docker image that runs Foursquare's Twofishes server with prebuilt index files.
 Exposes ports: 8080, 8081, 8082, 8083
 
 
-Instruction to run
+Instruction to run an hosted build
 ------------------
 
 docker run -d heyman/twofishes
@@ -26,3 +26,9 @@ unzipped first.
 The following files are downloaded:
 1. server-assembly-0.84.9.jar (downloaded from http://twofishes.net/binaries/server-assembly-0.84.9.jar)
 2. 2015-03-05-20-05-30.753698 (unzipped from http://twofishes.net/indexes/revgeo/2015-03-05.zip)
+
+
+Instruction to run your build
+------------------
+
+docker run -d twofishes:server_0.84.9-index_2015-03-05
--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-SERVER_ASSEMBLY_VERSION="server-assembly-0.84.9"
+###########CONFIGURE HERE###############
+
 INDEX_VERSION="2015-03-05"
+SERVER_ASSEMBLY_VERSION="0.84.9"
+
+###########END CONFIGURE HERE###########
+
+SERVER_ASSEMBLY="server-assembly-$SERVER_ASSEMBLY_VERSION"
 
 #get the dependencies
-wget -c http://twofishes.net/binaries/$SERVER_ASSEMBLY_VERSION.jar
+wget -c http://twofishes.net/binaries/$SERVER_ASSEMBLY.jar
 wget -c http://twofishes.net/indexes/revgeo/$INDEX_VERSION.zip
 
 #keep original server zip to avoid redownload
-cp $SERVER_ASSEMBLY_VERSION.jar build/server-assembly.jar 
+cp $SERVER_ASSEMBLY.jar build/server-assembly.jar 
 
 #unzip index file to build/index folder removing unneeded sublevel 
 unzip -d index $INDEX_VERSION.zip
@@ -18,7 +24,7 @@ mv -f $INDEX_EXTRACT_VERSION/ build/index/
 rmdir index
 
 #create text file vit used versions
-echo $SERVER_ASSEMBLY_VERSION > build/versions.txt
+echo $SERVER_ASSEMBLY > build/versions.txt
 echo $INDEX_EXTRACT_VERSION >> build/versions.txt
 
 RUN_CMD="java -Xmx2048m -jar build/server-assembly --hfile_basepath build/index/"
@@ -27,4 +33,4 @@ echo $RUN_CMD
 #$RUN_CMD
 
 cd build
-docker build .
+echo "docker build -t ""twofishes:server_$SERVER_ASSEMBLY_VERSION-index_$INDEX_VERSION"" ."
